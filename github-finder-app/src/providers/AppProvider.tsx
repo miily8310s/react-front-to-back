336bd9d3181060067db326f03a219a1bd24a27f2
miruoon <rillakerika@gmail.com>
2022-01-01 15:12:50 +0900

    feat/github-finder tailwindcssを追加

--- github-finder-app/src/providers/AppProvider.tsx
+++ github-finder-app/src/providers/AppProvider.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/components/ui/Footer";
 import { Navbar } from "@/components/ui/Navbar";
-import { Alert } from "@/components/functional/Alert";
-import { AlertProvider } from "@/context/alert";
+// import { Alert } from "@/components/functional/Alert";
+// import { AlertProvider } from "@/context/alert";
 import { GithubProvider } from "@/context/github";
 import { BrowserRouter as Router } from "react-router-dom";
 interface AppProviderProps {
